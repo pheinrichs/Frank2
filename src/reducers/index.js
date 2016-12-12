1f0a0c0140b26eed40e1e8ebcf7e0cd4f6996a3d
Scott MacLellan <s.j.maclell@gmail.com>
2016-12-11 20:27:41 -0500

    Full index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,3 +2,32 @@ import {combineReducers} from 'redux';
 
 export default combineReducers({
 });
+
+// States
+/*
+{
+  music: {
+    timers
+  },
+  players: {
+    p1: {
+      health: 10,
+      bullets: 12
+    },
+    p2: {
+      health: 10,
+      bullets: 12
+    }
+  },
+  actions: {
+    visible: true
+    p1: {
+      type: 'shoot' | 'block' | 'reload'
+      success: true || false
+    },
+    p2: {
+      ...
+    }
+  }
+}
+*/
